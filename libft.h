5fe8994e0c5ff1a706a4bdbf58fc2593bc8875e2
deveshaha <devesh_hanumante@outlook.com>
2023-03-23 17:08:44 +0000

    tests

--- libft.h
+++ libft.h
@@ -20,15 +20,15 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char *s);
-//void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-//void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+//void		*ft_memset(void *b, int c, size_t len);
+void		ft_bzero(void *s, size_t n);
+//void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 //memmove
 //strlcpy
 //strlcat
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
+char		*ft_strchr(const char *s, int c);
 //strncmp
 //memchr
 //memcmp
