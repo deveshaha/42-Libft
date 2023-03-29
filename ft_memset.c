32a2e4582c4c55a1ab821dc718291ebec14ae703
deveshaha <devesh_hanumante@outlook.com>
2023-03-29 17:24:06 +0200

    ft_memset.c tested with my own tests

--- ft_memset.c
+++ ft_memset.c
@@ -10,23 +10,64 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	int				i;
-	int				l;
-	unsigned char	*s;
+	unsigned char	*str;
+	size_t			i;
 
+	str = (unsigned char *) s;
 	i = 0;
-	l = len;
-	s = b;
-	while (s[i] != '\0' && (i < l))
+	while (i < n)
 	{
-		s[i] = c;
+		str[i] = c;
 		i++;
 	}
-	return (s);
+	return (str);
 }
+
+/*
+
+int main(void)
+{
+    char str[50] = "GeeksForGeeks is for programming geeks.";
+	printf("Original function:");
+    printf("\nBefore memset(): %s\n", str);
+  
+    // Fill 8 characters starting from str[13] with '.'
+    memset(str, '.', 8);
+  
+    printf("After memset():  %s", str);
+
+	printf("\n\nMy function:");
+	printf("\nBefore memset(): %s\n", str);
+
+	ft_memset(str, '.', 8);
+
+	printf("After memset():  %s", str);
+	
+
+	//TEST 2:
+    int n = 10;
+    int arr[n];
+  
+    // Fill whole array with 0.
+	printf("\nOriginal function:");
+    memset(arr, 0, n*sizeof(arr[0]));
+    printf("\nArray after memset()\n");
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+
+	printf("\n\nMy function:");
+	ft_memset(arr, 0, n*sizeof(arr[0]));
+	printf("\nArray after memset()\n");
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+
+    return (0);
+}
+
 */
